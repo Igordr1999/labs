cfdcfb28e7c64e69b5153fa45ed2fe837bef06db
Igordr1999 <Igordr1999@yandex.ru>
2017-09-29 01:19:16 +0300

    Add Reverse Polish notation

---
+++ practical work/Reverse Polish notation/Source.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<stack>
+using namespace std;
+
+int calc(int x, int y, char t) {
+	switch (t) {
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	default:
+		break;
+	}
+}
+
+int main() {
+	stack <int> st;
+	int n, q, a, b, value;
+	char s;
+	cout << "lenght: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if ((s == '+') || (s == '-') || (s == '*') || (s == '/')) {
+			//запоминаем и удаляем два верхних числа из стека
+			b = st.top();
+			st.pop();
+			a = st.top();
+			st.pop();
+
+			//вычисляем результат и добавляем его в стек
+			value = calc(a, b, s);
+			st.push(value);
+			cout << a << " " << s << " " << b << " = " << value << endl;
+		}
+		else {
+			//добавляем число (цифру) в стек
+			q = (int)s - 48;
+			st.push(q);
+		}
+	}
+	cout << endl << "Answer: " << value<<endl;
+	system("pause");
+	return 0;
+}
