fa42eb1359ab3439422aa01c6e910871af3b52b1
Igordr1999 <Igordr1999@yandex.ru>
2017-10-27 10:16:47 +0300

    Update mystack.c

--- mystack.c
+++ mystack.c
@@ -7,24 +7,36 @@ int count = 0;
 int my_stack[MY_SIZE];
 
 int check_full() {
-	if (count == MY_SIZE) return 1;
-	else return 0;
+	if (count == MY_SIZE){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 int check_empty() {
-	if (count == 0) return 1;
-	else return 0;
+	if (count == 0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 void push(int n) {
-	if (check_full() == 1) return;
+	if (check_full() == 1) {
+		return;
+	}
 	my_stack[count] = n;
 	count++;
 	return;
 }
 
 void pop() {
-	if (check_empty() == 1) return;
+	if (check_empty() == 1){
+		return;
+	}
 	my_stack[count - 1] = 0;
 	count--;
 }
