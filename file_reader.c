be400df81f5d18df5c6a563240c8d879fac18d50
Igordr1999 <Igordr1999@yandex.ru>
2017-12-15 13:08:28 +0300

    Update file_reader.c

--- file_reader.c
+++ file_reader.c
@@ -1,37 +1,87 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+#include <malloc.h>
 
-int main() {
-	FILE *file;
-	char file_name[30];
-	scanf("%s", &file_name);
-	file = fopen(file_name, "r");
-	if (file == NULL) {
-		while (file == NULL){
-			printf("%s", "FILE NOT FOUND. Please, try again\n");
-			scanf("%s", &file_name);
-			file = fopen(file_name, "r");
-		}
-	}
-	
-	char text[1000];
-	int i = 0;
-	char *real_tail;
+int BMsearch(char str, const char word) {
+    int N=strlen(str);
+    int M=strlen(word) -1;
 
-	while (fgets(text, sizeof(text), file)){
-		real_tail = "";
+    int d[256];
+    int i;
+    for (i = 0; i<256; i++)
+        d[i] = M;
 
+    for (i = 0; i<M - 1; i++)
+        d[(unsigned char) word[i]] = M - i - 1;
 
-		if (text[strlen(text) - 1] == '\n')
-		{
-			real_tail = "\\n";
-			text[strlen(text) - 1] = '\0';
-		};	
-		printf("%s\n", text);
-	}
-	int size = ftell(file);
-	printf("File size = %d bytes\n", size);
-	fclose(file);
-	return 0;
+    int result = -1;
+    for (i = M; i = N; i += d[(unsigned char) str[i - 1]]) {
+        int j, k;
+        for (j = M - 1, k = i - 1; j = 0 && str[k] == word[j]; k--, j--);
+        if (j  0) {
+            result = i - M;
+            break;
+        }
+        if (i == N) {
+            result = -1;
+            break;
+        }
+    }
+    return result;
+}
+
+int main(int argc, char argv[]) {
+
+    FILE file = NULL;
+    size_t size = sizeof(char)  2048;
+    char filename = NULL;
+
+    if (argc  2  (file = fopen(argv[1], r) ) == NULL) {
+        while (!file) {
+            printf(Write filenamen);
+            getline(&filename, &size, stdin); DGitHubRepLab-WorkspaceSearchInFiletest.txt
+
+            filename[strlen(filename)-1]= '0';
+            file = fopen(filename, r);
+
+            if (file == NULL) {
+                printf(Error the file not found!n);
+            }
+        }
+        free(filename);
+    }
+    printf(Success File opened!n);
+
+    int row = 0;
+    int column = -1;
+    char line = malloc(size);
+    char input = NULL;
+
+    if (argc  2) {
+        input = argv[2];
+    } else {
+        printf(Write string for searchn);
+        getline(&input, &size, stdin);
+    }
+
+    printf(Success Start searching...n);
+
+    while (fgets(line, size, file) != NULL) {
+        row++;
+        column = BMsearch(line, input);
+
+        if (column != -1)
+            break;
+    }
+
+    if (column == -1) {
+        printf(Not foundn);
+    } else {
+        printf(File string%sYour string%sn, line, input);
+        printf(Row %dnColumn %dn, row, ++column);
+    }
+    free(line);
+    free(input);
+    fclose(file);
+    return 0;
 }
