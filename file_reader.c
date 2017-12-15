61004cb567376a7771d711b9bc0ecff192a55a92
Igordr1999 <Igordr1999@yandex.ru>
2017-12-15 13:53:54 +0300

    Update file_reader.c

--- file_reader.c
+++ file_reader.c
@@ -1,87 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-int BMsearch(char str, const char word) {
-    int N=strlen(str);
-    int M=strlen(word) -1;
-
-    int d[256];
-    int i;
-    for (i = 0; i<256; i++)
-        d[i] = M;
-
-    for (i = 0; i<M - 1; i++)
-        d[(unsigned char) word[i]] = M - i - 1;
-
-    int result = -1;
-    for (i = M; i = N; i += d[(unsigned char) str[i - 1]]) {
-        int j, k;
-        for (j = M - 1, k = i - 1; j = 0 && str[k] == word[j]; k--, j--);
-        if (j  0) {
-            result = i - M;
-            break;
-        }
-        if (i == N) {
-            result = -1;
-            break;
-        }
-    }
-    return result;
+size_t getFileSize(const char* filename)
+{
+struct stat st;
+if(stat(filename, &st) != 0)
+	{
+	return 0;
+	}
+return st.st_size;
 }
 
-int main(int argc, char argv[]) {
+int main(int argc, char **argv)
+{
+FILE *fp;
+char c;
+int size;
 
-    FILE file = NULL;
-    size_t size = sizeof(char)  2048;
-    char filename = NULL;
+fp = fopen(argv[1], "r");
 
-    if (argc  2  (file = fopen(argv[1], r) ) == NULL) {
-        while (!file) {
-            printf(Write filenamen);
-            getline(&filename, &size, stdin); DGitHubRepLab-WorkspaceSearchInFiletest.txt
+if (fp)
+	{
+	while ((c = getc(fp)) != EOF)
+	putchar(c);
 
-            filename[strlen(filename)-1]= '0';
-            file = fopen(filename, r);
+	printf("\n");
+	size = getFileSize(argv[1]);
+	printf ("%20s has %d bytes.\n", argv[1], size);
+	}
+else
+printf("Please, provide a valid file name.\n");
 
-            if (file == NULL) {
-                printf(Error the file not found!n);
-            }
-        }
-        free(filename);
-    }
-    printf(Success File opened!n);
-
-    int row = 0;
-    int column = -1;
-    char line = malloc(size);
-    char input = NULL;
-
-    if (argc  2) {
-        input = argv[2];
-    } else {
-        printf(Write string for searchn);
-        getline(&input, &size, stdin);
-    }
-
-    printf(Success Start searching...n);
-
-    while (fgets(line, size, file) != NULL) {
-        row++;
-        column = BMsearch(line, input);
-
-        if (column != -1)
-            break;
-    }
-
-    if (column == -1) {
-        printf(Not foundn);
-    } else {
-        printf(File string%sYour string%sn, line, input);
-        printf(Row %dnColumn %dn, row, ++column);
-    }
-    free(line);
-    free(input);
-    fclose(file);
-    return 0;
+fclose(fp);
 }
