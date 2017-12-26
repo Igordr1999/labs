6aa9ca83f111e79f47515a5f4e426bc833ea4434
Igordr1999 <Igordr1999@yandex.ru>
2017-12-26 20:16:07 +0300

    Update converter.c

--- converter.c
+++ converter.c
@@ -5,9 +5,13 @@ int main(int argc, char* argv[]) {
     if(argc < 2){
         return 0;
     }
+    
 
     if(argc == 2){
         double temp = atof(argv[1]);
+        if(temp < 273.15){
+            return 1;
+        }
         printf("%.2f %s", temp, "C\n");
         printf("%.2f %s", temp + 273.15, "K\n");
         printf("%.2f %s", 9 * (temp + 32) / 5, "F\n\n");
@@ -16,6 +20,9 @@ int main(int argc, char* argv[]) {
     if(argc == 3){
         double temp = atof(argv[1]);
         char scale = *argv[2];
+        if(((temp < 273.15) && (scale == 'C')) || ((temp <= -459,67) && (scale == 'F')) || ((temp <= 0) && (scale == 'K'))){
+            return 1;
+        }
         switch (scale) {
             case 'C':
                 printf("%.2f %s", temp, "C\n");
